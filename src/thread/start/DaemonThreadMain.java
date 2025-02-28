ef545f1ac6fb25d087ebb3a754375f7b2a719ac5
승호 <tmdgh717@naver.com>
2025-02-28 14:37:35 +0900

    데몬 스레드

---
+++ src/thread/start/DaemonThreadMain.java
@@ -0,0 +1,28 @@
+package thread.start;
+
+public class DaemonThreadMain {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        DaemonThread daemonThread = new DaemonThread();
+        daemonThread.setDaemon(true); //데몬 스레드 여부, false가 기본값으로 false 일 경우 사용자 스레드(User Thread)
+        daemonThread.start();
+
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+
+    static class DaemonThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println(Thread.currentThread().getName() + ": run()");
+
+            try {
+                Thread.sleep(10000); //10초간 실행
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println(Thread.currentThread().getName() + ": run() end");
+        }
+    }
+}
