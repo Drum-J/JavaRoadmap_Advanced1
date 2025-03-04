c60cfcda0d09f8c8e7b13b26f2764f1d0eef1abb
승호 <tmdgh717@naver.com>
2025-03-04 10:04:02 +0900

    인터럽트 - 시작1

---
+++ src/thread/control/interrupt/ThreadStopMainV1.java
@@ -0,0 +1,31 @@
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV1 {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread thread = new Thread(task, "work");
+        thread.start();
+
+        sleep(4000);
+        log("작업 중단 지시 runFlag = false"); //작업 중단 지시 후 MyTask 스레드가 sleep 상태기 때문에 약 2초후에 작업이 종료된다.
+        task.runFlag = false;
+    }
+
+    static class MyTask implements Runnable {
+
+        volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            while (runFlag) {
+                log("작업 중");
+                sleep(3000);
+            }
+            log("자원 정리");
+            log("자원 종료");
+        }
+    }
+}
