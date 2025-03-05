ea935bb8fd03b13373981a7aac69f174f0b62655
승호 <102205699+Drum-J@users.noreply.github.com>
2025-03-05 10:40:58 +0900

    04.메모리 가시성 완료 (#4)
    * volatile, 메모리 가시성1
    
    * volatile, 메모리 가시성2
    
    * volatile, 메모리 가시성3

---
+++ src/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,34 @@
+package thread.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag = " + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        log("runFlag를 false로 변경 시도");
+        task.runFlag = false;
+        log("runFlag = " + task.runFlag);
+        log("main 종료");
+    }
+
+    static class MyTask implements Runnable {
+
+        //boolean runFlag = true; // 캐시 메모리를 사용
+        volatile boolean runFlag = true; // 메인 메모리에 직접 접근, 캐시 메모리를 사용할 때 보다 성능은 떨어짐
+
+        @Override
+        public void run() {
+            log("task 시작");
+            while (runFlag) {
+                // runFlag가 false로 변하면 탈출
+            }
+            log("task 종료");
+        }
+    }
+}
